f5c5d63dbb1505cb1041f2b6062982e346c9900a
Chan Seob Park <55018831+monocsp@users.noreply.github.com>
2023-05-30 16:13:45 +0900

    [update] 20230530 toss_payment webview_flutter
    version update
    gradle update

--- toss_payment/lib/feature/payments/webview/payment_webview.dart
+++ toss_payment/lib/feature/payments/webview/payment_webview.dart
@@ -1,7 +1,6 @@
 import 'dart:developer' as dev;
 import 'dart:io';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:toss_payment/extensions/uri_extension.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -42,6 +41,45 @@ class PaymentWebView extends StatefulWidget {
 }
 
 class _PaymentWebViewState extends State<PaymentWebView> {
+  WebViewController _webViewController = WebViewController();
+
+  @override
+  void initState() {
+    _webViewController = WebViewController()
+      ..loadRequest(Uri.parse(widget.paymentRequestUrl.toString()))
+      ..setNavigationDelegate(NavigationDelegate(
+        onPageStarted: widget.onPageStarted,
+        onPageFinished: widget.onPageFinished,
+        onNavigationRequest: (request) async {
+          Uri uri = Uri.parse(request.url);
+          // url 이 웹뷰에 유효하다면 해당 url 로 이동한다.
+          if (uri.scheme == 'http' ||
+              uri.scheme == 'https' ||
+              uri.scheme == 'about') {
+            return NavigationDecision.navigate;
+          }
+
+          // url 이 웹뷰에 유효하지 않다면 해당 url 로 이동하지 않는다.
+          dev.log("NavigationDecision = ${request.url}",
+              name: "WebView.navigationDelegate");
+
+          String launchUrl = request.url;
+
+          if (Platform.isAndroid) {
+            Uri tossPayment = UriExtension.androidIntentUrlOrigin(request.url);
+            launchUrl = tossPayment.toString();
+          }
+
+          launch(launchUrl);
+
+          return NavigationDecision.prevent;
+        },
+      ))
+      ..setJavaScriptMode(JavaScriptMode.unrestricted);
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -67,36 +105,7 @@ class _PaymentWebViewState extends State<PaymentWebView> {
           )
         ],
       ),
-      body: WebView(
-        initialUrl: widget.paymentRequestUrl.toString(),
-        onPageStarted: widget.onPageStarted,
-        onPageFinished: widget.onPageFinished,
-        navigationDelegate: (request) async {
-          Uri uri = Uri.parse(request.url);
-          // url 이 웹뷰에 유효하다면 해당 url 로 이동한다.
-          if (uri.scheme == 'http' ||
-              uri.scheme == 'https' ||
-              uri.scheme == 'about') {
-            return NavigationDecision.navigate;
-          }
-
-          // url 이 웹뷰에 유효하지 않다면 해당 url 로 이동하지 않는다.
-          dev.log("NavigationDecision = ${request.url}",
-              name: "WebView.navigationDelegate");
-
-          String launchUrl = request.url;
-
-          if (Platform.isAndroid) {
-            Uri tossPayment = UriExtension.androidIntentUrlOrigin(request.url);
-            launchUrl = tossPayment.toString();
-          }
-
-          launch(launchUrl);
-
-          return NavigationDecision.prevent;
-        },
-        javascriptMode: JavascriptMode.unrestricted,
-      ),
+      body: WebViewWidget(controller: _webViewController),
     );
   }
 
@@ -105,6 +114,7 @@ class _PaymentWebViewState extends State<PaymentWebView> {
     if (widget.onDisposed != null) {
       widget.onDisposed!();
     }
+
     super.dispose();
   }
 }
